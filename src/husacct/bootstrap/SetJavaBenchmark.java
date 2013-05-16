90a5994bfc361cc91f124ed574b93a574848590b
Twan <twanverboom@outlook.com>
2013-05-17 01:48:35 +0200

    Changes in bootstrap to support parameters.
    Paths to a test project can be passed by adding ?"path1"|"path2"|"path3"
    after ,SetJavaBenchmark.
    The | allows you to add more paths but this is optional.
    
    Software units can be mapped by adding
    ?"softwareUnit1">"PACKAGE"|"softwareUnit"|"SoftwareUnit">"type2".
    Here you can use the | as a separator as well.
    The > is optional. It maps the type, however if left out it will use
    "PACKAGE" by default.

--- src/husacct/bootstrap/SetJavaBenchmark.java
+++ src/husacct/bootstrap/SetJavaBenchmark.java
@@ -7,16 +7,31 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class SetJavaBenchmark extends AbstractBootstrap{
+	private String[] pathsToCustomTestProject = {};
 	
 	@Override
 	public void execute() {
 		ArrayList<ProjectDTO> projects = new ArrayList<ProjectDTO>();
 		ArrayList<String> paths = new ArrayList<String>();
-		paths.add(new File("").getAbsolutePath() + File.separator + "testprojects" + File.separator + "java" + File.separator + "benchmark");
+		
+		for(String pathToCustomTestProject : pathsToCustomTestProject){
+			if(!new File(pathToCustomTestProject).isDirectory()){
+				paths.add(new File("").getAbsolutePath() + File.separator + "testprojects" + File.separator + "java" + File.separator + "benchmark");
+			}else{
+				paths.add(pathToCustomTestProject);
+			}
+		}
+		
 		ArrayList<AnalysedModuleDTO> analysedModules = new ArrayList<AnalysedModuleDTO>();
 		ProjectDTO project = new ProjectDTO("Java Benchmark", paths, "Java", "1.0", "Benchmark Project", analysedModules);
 		projects.add(project);
 		getDefineService().createApplication("Java Benchmark", projects, "1.0");
+	}
+
+	@Override
+	public void execute(String[] args) {
+		pathsToCustomTestProject = args;
+		execute();
 	}
 
 }
